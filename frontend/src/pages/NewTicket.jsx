c4f94028c01b131542ca5df6b303220332168fd7
maoo abdalla <maoo@bintzone.com>
2023-11-13 02:26:14 +0300

    mam

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -96,12 +96,10 @@ function NewTicket() {
                 purchase date
               </label>
               <input
+                id='dateId'
                 type='date'
-                className='arriveDate all-form-input all-H-25px all-fz-18px all-NT-input all-'
-                onChange={onChange}
-                value={purchase_date}
-                id='purchase_date'
-                required
+                placeholder='yyyy-mm-dd'
+                pattern='\d{4}-\d{2}-\d{2}'
               />
             </div>
             {/* serial */}
